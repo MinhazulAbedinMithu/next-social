f30ae6d3ac82edd6dec9d33eaeb592c5b1d7073c
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2022-02-13 20:12:42 +0600

    err: tailwind did not working at NewFeedssss

--- utilities/NewFeeds.tsx
+++ utilities/NewFeeds.tsx
@@ -6,7 +6,7 @@ import NewFeedsItem from "./NewFeedsItem";
 
 const NewFeeds = () => {
 	return (
-		<div className="">
+		<div className="w-full bg-red-400">
 			<h2 className="text-3xl text-red-400	">New Feeds</h2>
 			<NewFeedsItem Icon={CgFeed} title="New Feeds" />
 			<NewFeedsItem Icon={RiProfileFill} title="Profile" />
--- utilities/NewFeedsItem.tsx
+++ utilities/NewFeedsItem.tsx
@@ -2,7 +2,7 @@ const NewFeedsItem = (props: any) => {
 	const { Icon, title } = props;
 	return (
 		<div>
-			<div className="flex items-center">
+			<div className="flex items-center bg-red-700">
 				<Icon /> <span>{title}</span>
 			</div>
 		</div>
